b5c5f20e3ae60cf3c55c5a24c7c5070df1db8b76
Bojan Brankovic <bojanbrankovic85@yahoo.com>
2026-05-03 18:59:11 +0200

    Update test_login.py

--- python-playwright-ui-automation/tests/test_login.py
+++ python-playwright-ui-automation/tests/test_login.py
@@ -9,10 +9,25 @@ def test_login_success():
         login_page = LoginPage(page)
 
         login_page.open()
-        login_page.login("standard_user", "secret_sauce")
+        login_page.login("tomsmith", "SuperSecretPassword!")
 
-        assert "inventory" in page.url
+        assert login_page.is_login_successful()
 
         page.screenshot(path="screenshots/login-success.png")
+
+        browser.close()
+
+
+def test_login_fail():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        login_page = LoginPage(page)
+
+        login_page.open()
+        login_page.login("wrong", "wrong")
+
+        assert login_page.is_login_failed()
 
         browser.close()
