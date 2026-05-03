e172a8b74dcc3f5f0b2e8e6bc8ca3e47b32fa340
Bojan Brankovic <bojanbrankovic85@yahoo.com>
2026-05-03 18:58:22 +0200

    Update login_page.py

--- python-playwright-ui-automation/pages/login_page.py
+++ python-playwright-ui-automation/pages/login_page.py
@@ -3,14 +3,22 @@ from playwright.sync_api import Page
 class LoginPage:
     def __init__(self, page: Page):
         self.page = page
-        self.username_input = "#user-name"
+        self.username_input = "#username"
         self.password_input = "#password"
-        self.login_button = "#login-button"
+        self.login_button = "button[type='submit']"
+        self.success_message = ".flash.success"
+        self.error_message = ".flash.error"
 
     def open(self):
-        self.page.goto("https://www.saucedemo.com/")
+        self.page.goto("https://the-internet.herokuapp.com/login")
 
     def login(self, username: str, password: str):
         self.page.fill(self.username_input, username)
         self.page.fill(self.password_input, password)
-        self.page.click(self.login_button)
+        self.page.locator(self.login_button).click()
+
+    def is_login_successful(self):
+        return self.page.locator(self.success_message).is_visible()
+
+    def is_login_failed(self):
+        return self.page.locator(self.error_message).is_visible()
